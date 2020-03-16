d5181689c81953aba7ce63d2e5d4090c09319d45
Avinash Tiwari <avitiw@gmail.com>
2020-03-16 04:12:17 +0000

    Statement creation

--- src/Common.FEEL.Engine/Library.fs
+++ src/Common.FEEL.Engine/Library.fs
@@ -1,5 +1,47 @@
 ﻿namespace Common.FEEL.Engine
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+module Expression =
+    type  IgnoreStatement = private {
+        id : string
+    }
+
+    type EqualsStatement =
+     private {
+        id : string
+        right : string
+     }
+     member self.Right() = self.right
+     
+    type  Statement =
+        | Ignore of IgnoreStatement 
+        | Equals of  EqualsStatement
+        member self.Id() =
+            match self with 
+            | Ignore i -> i.id
+            | Equals e -> e.id
+    module IgnoreStatement =
+        let create id  : Result<Statement,string>=
+            match (id |>  String.IsNullOrWhiteSpace) with
+              | true -> Error "Id cannot be empty"
+              | _ -> Ignore  { id = id} |> Ok
+
+    module EqualsStatement =
+        let create id rightOperand  =
+            match (id |>  String.IsNullOrWhiteSpace) with
+              | true -> Error "Id cannot be empty"
+              | _ -> Equals  {id = id; right = rightOperand} |> Ok
+       
+
+    
+   
+    
+    
+    
+    type Expression = {
+        statements : Statement seq
+        inputs : string seq
+        outputs : string seq
+    }
+
+    
--- test/Common.FEEL.Engine.Tests/Tests.fs
+++ test/Common.FEEL.Engine.Tests/Tests.fs
@@ -2,7 +2,30 @@ module Tests
 
 open System
 open Xunit
+open Common.FEEL.Engine.Expression
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Create Ignore Statement`` () =
+    let res = IgnoreStatement.create "r01"
+    Assert.NotNull(res)
+    match res with  
+        | Error e -> failwith e
+        | Ok stmt -> 
+            Assert.NotNull stmt            
+            Assert.Equal("r01",stmt.Id())
+
+ 
+[<Fact>]
+let ``Create Equals Statement`` () =
+    let res = EqualsStatement.create "r02" "Car"
+    Assert.NotNull (res)
+    match res with  
+        | Error e -> failwith e
+        | Ok stmt -> 
+            Assert.NotNull stmt
+            Assert.Equal("r02",stmt.Id())   
+            match stmt with
+                | Equals e ->   Assert.Equal("Car",e.Right())
+                | _ -> Assert.True(true)
+
+    
